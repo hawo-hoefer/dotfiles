de58c2da69e4e55263010bedb467b1e7883f0ebf
Hawo Höfer <hawo.hoefer98@freenet.de>
2025-11-16 16:39:57 +0100

    improve wacom setup

--- qtile/wacom_setup.sh
+++ qtile/wacom_setup.sh
@@ -9,9 +9,11 @@ display_cfg=$(autorandr --current 2> /dev/null)
 for size in ${sizes[@]}; do
   # Active Pen Region
   if [[ $display_cfg == "home" ]]; then
-    xsetwacom set "Wacom Intuos BT $size Pen stylus" MapToOutput 3840x2160+2560+0
+    xsetwacom set "Wacom Intuos BT $size Pen stylus" MapToOutput 3840x2160+3840+0
   elif [[ $display_cfg == "work" ]]; then
     xsetwacom set "Wacom Intuos BT $size Pen stylus" MapToOutput 2560x1440+2560+0
+  elif [[ $display_cfg == "" ]]; then
+    xsetwacom set "Wacom Intuos BT $size Pen stylus" MapToOutput 3840x2160+3840+0
   else
     xsetwacom set "Wacom Intuos BT $size Pen stylus" MapToOutput 2560x1600+0+0
   fi
